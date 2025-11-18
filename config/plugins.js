9a8a93bafa7464e95c6abf2576669db0704dee6d
Tsvetan Stoyanov <tsvetan.stoyanov@plainsea.com>
2025-11-18 10:17:15 +0200

    Add strapi-cache

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,18 @@
-module.exports = () => ({});
+module.exports = {
+    'strapi-cache': {
+        enabled: true,
+        config: {
+            debug: false,
+            max: 1000,
+            ttl: 1000 * 60 * 60,
+            size: 1024 * 1024 * 1024,
+            allowStale: false,
+            cacheableRoutes: ['/api/tags', '/api/articles', '/api/categories'], // Routes to cache
+            excludeRoutes: [],
+            provider: 'memory',
+            cacheHeaders: true,
+            cacheHeadersDenyList: ['access-control-allow-origin', 'content-encoding'],
+            cacheAuthorizedRequests: false,
+        },
+    },
+};
